1e65ebe67d8ee93224a5f327c147c09931ed1be5
coderamins <raminsolhi1@gmail.com>
2025-09-08 17:50:19 +0330

    140406170550

---
+++ src/Modules/Catalog/ShopX.Catalog.Application/Products/DTOs/CreateProductResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopX.Catalog.Application.Products.DTOs
+{
+    public class CreateProductResponse
+    {
+        public Guid Id { get; set; }
+    }
+}
