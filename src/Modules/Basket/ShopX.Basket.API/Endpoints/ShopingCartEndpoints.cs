2a2e1d9f017fac89b45f592ce78a40e61301f026
coderamins <raminsolhi1@gmail.com>
2025-10-05 18:15:26 +0330

    140407130615

---
+++ src/Modules/Basket/ShopX.Basket.API/Endpoints/ShopingCartEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace ShopX.Basket.API.Endpoints
+{
+    public static class ShopingCartEndpoints
+    {
+        public static IEndpointRouteBuilder MapShopingCartEndpoints(this IEndpointRouteBuilder app)
+        {
+            var group = app.MapGroup("/api/v1/basket")
+                .WithTags("ShopingCart");
+
+            group.MapGet("/buyerId", async (string buyerId, ISender sender) =>
+            {
+                var cart = await sender.Send(new GetShoppingCartQuery(buyerId));
+                return cart is null Results.NotFound():Results.Ok();
+            });
+        }
+    }
+}
---
+++ src/Modules/Basket/ShopX.Basket.Infrastructure/Persistence/BasketDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopX.Basket.Domain.Entities;
+
+namespace ShopX.Basket.Infrastructure.Persistence
+{
+    public class BasketDbContext:DbContext
+    {
+        public BasketDbContext(DbContextOptions<BasketDbContext> options):base(options) { }
+
+        public DbSet<ShopingCart> ShopingCarts => Set<ShopingCart>();
+        public DbSet<ShopingCartItem> ShopingCartItems => Set<ShopingCartItem>();
+
+    }
+}
