39517217cd3ce100ec3b35f3ef46cde9c855c577
coderamins <raminsolhi1@gmail.com>
2025-10-13 17:45:37 +0330

    140407210545

---
+++ src/Modules/Identity/ShopX.Identity.Infrastructure/Data/DesignTimeIdentityDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ShopX.Identity.Infrastructure.Data
+{
+    public class DesignTimeIdentityDbContextFactory
+          : IDesignTimeDbContextFactory<IdentityDbContext>
+    {
+        public IdentityDbContext CreateDbContext(string[] args)
+        {
+            // مسیر فایل appsettings.json در پروژه‌ی API
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../ShopX.Identity.API");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var connectionString = config.GetConnectionString("IdentityDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<IdentityDbContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new IdentityDbContext(optionsBuilder.Options);
+        }
+    }
+}
