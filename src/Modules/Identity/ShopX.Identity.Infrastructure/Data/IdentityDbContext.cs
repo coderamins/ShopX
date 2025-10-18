5e1ad524f565331774ca22cea9830fafd4c8f7fd
coderamins <raminsolhi1@gmail.com>
2025-10-18 17:51:51 +0330

    140407260551

--- src/Modules/Identity/ShopX.Identity.Infrastructure/Data/IdentityDbContext.cs
+++ src/Modules/Identity/ShopX.Identity.Infrastructure/Data/IdentityDbContext.cs
@@ -10,5 +10,11 @@ namespace ShopX.Identity.Infrastructure.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
